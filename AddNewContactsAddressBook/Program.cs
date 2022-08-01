380ee659b692b6ccdfcaa7362a1176e6e09c4d98
Aman <amankr97111@gmail.com>
2022-08-01 05:37:59 +0530

    first commit

---
+++ AddNewContactsAddressBook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddNewContactsAddressBook
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressManagment addressmanagment = new AddressManagment();
+            int option;
+            do
+            {
+                Console.WriteLine("Please choose number as shown below\n1:Add Contacts.\n2:Display Contacts\n3:Edit Contacts.\n4:Exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressmanagment.AddContacts();
+                        break;
+                    case 2:
+                        addressmanagment.DisplayContacts();
+                        break;
+                    case 3:
+                        addressmanagment.EditContacts();
+                        break;
+                    case 4:
+                       
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+
+            } while (option != 4);
+            Console.ReadLine();
+        }
+    }
+}
